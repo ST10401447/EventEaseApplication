d039f7266a35a3683f9190304e7c3e63516f3637
ST10401447 <131337974+ST10401447@users.noreply.github.com>
2026-05-08 21:57:10 +0200

    Add files via upload

---
+++ EventType.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApplication.Models
+{
+    public class EventType
+    {
+        [Key]
+        public int EventTypeID { get; set; }
+
+        [Required]
+        public string EventTitle { get; set; }
+
+        public ICollection<Event>? Events { get; set; }
+    }
+}
---
+++ VenueType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApplication.Models
+{
+    public class VenueType
+    {
+        [Key]
+        public int VenueTypeID { get; set; }
+
+        [Required]
+        public string VenueTitle { get; set; }
+
+        public ICollection<Venue>? Venues { get; set; }
+    }
+}
